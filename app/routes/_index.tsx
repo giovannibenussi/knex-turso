7942b9b722dfbf0de46b536ef35132a5cf09b5fa
Giovanni Benussi <giovanni.benussi@usach.cl>
2024-01-23 16:01:22 -0300

    working example

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,28 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type MetaFunction } from "@remix-run/node";
+import { knexClient } from "./db.server";
+
+import { createClient } from "@libsql/client";
+import { useLoaderData } from "@remix-run/react";
+
+const client = createClient({
+  url: "libsql://test-giovannibenussi.turso.io",
+  authToken:
+    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJnaWQiOiJlODQwODFhYy1iOWRmLTExZWUtYmJjYy0xNmRjNDNjNjljODYiLCJpYXQiOiIyMDI0LTAxLTIzVDE1OjM2OjA0LjcyODY5NzI5OFoifQ.N1dOYDPk9YBUEckgeGIaEOGO-qlvaBI_Xyvl8lsz7_n_w0EH4aFxkWWYOl0IbaAJvrvVpBCLIcpLa0idatLiAQ",
+});
+
+type User = {
+  id: string;
+  first_name: string;
+};
+
+export const loader = async () => {
+  const result = await client.execute("SELECT * FROM users");
+  const users = result.rows as unknown as User[];
+  const knexUsers: User[] = await knexClient<User[]>("users").select(
+    "first_name"
+  );
+  return json({ ok: true, users, knexUsers });
+};
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,9 +32,19 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const { users, knexUsers } = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <h2>LibSQL Users</h2>
+      {users.map((user) => (
+        <p key={user.first_name}>{user.first_name}</p>
+      ))}
+      <h2>Knex Users</h2>
+      {knexUsers.map((user) => (
+        <p key={user.first_name}>{user.first_name}</p>
+      ))}
       <ul>
         <li>
           <a
---
+++ app/routes/db.server.ts
@@ -0,0 +1,9 @@
+import knex from "knex";
+
+export const knexClient = knex({
+  client: "sqlite3",
+  connection: {
+    filename:
+      "libsql://test-giovannibenussi.turso.io?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIyMDI0LTAxLTIzVDE4OjU3OjA3LjkzMjc4MjM5NVoiLCJpZCI6ImYyNTRkNDE0LWI5ZGYtMTFlZS1iYmNjLTE2ZGM0M2M2OWM4NiJ9.gJMHd3kHZ-M8h3Zwa8KhFN3XyZkp1aDOUzz7drvjqQKlcZ7BmfZ0RaKnPdb3Qj3nTCQgBha0Izb9N3quZjExDg",
+  },
+});
